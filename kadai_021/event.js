2ef31a7d434b15742753b07acbe0fafa4eab3722
Ahilo9 <148359025+Ahilo9@users.noreply.github.com>
2023-11-20 23:49:25 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,8 @@
+setTimeout(() => {
+  const change = document.getElementById('btn');
+  change.addEventListener('click', ()=>{
+  const content = document.getElementById('text');
+});
+text.textContent  = "ボタンをクリックしました";
+}, 2000);
+
