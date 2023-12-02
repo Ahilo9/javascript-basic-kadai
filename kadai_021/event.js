57d797ea798c41330099cfe41b71d04cc0257536
Ahilo9 <148359025+Ahilo9@users.noreply.github.com>
2023-12-02 14:06:20 +0900

    Delete kadai_021 directory

--- kadai_021/event.js
+++
@@ -1,8 +0,0 @@
-setTimeout(() => {
-  const change = document.getElementById('btn');
-  change.addEventListener('click', ()=>{
-  const content = document.getElementById('text');
-});
-text.textContent  = "ボタンをクリックしました";
-}, 2000);
-
